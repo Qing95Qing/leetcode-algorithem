6bb1a1b34dac62910911615c472ebed9e975ce93
qing.qing <849832144@qq.com>
2024-05-16 18:41:55 +0800

    560/semi-resolved

--- 560-subArraySumK.js
+++ 560-subArraySumK.js
@@ -2,7 +2,12 @@
  * @lc app=leetcode.cn id=560 lang=javascript
  *
  * [560] 和为 K 的子数组
+ * [3, 2, 4, 2, 3, 6]
+ * 
+ * [3, 5]
+ * [0, 2]
  */
+
 
 // @lc code=start
 /**
@@ -11,7 +16,26 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-
+    const preSum = new Array(nums.length).fill(0).map(() => new Array(nums.length).fill(0));
+    preSum[0][0] = nums[0];
+    let counter = 0;
+    if (preSum[0][0] === k) {
+        counter += 1;
+    }
+    
+    for (let i = 1; i < nums.length; i++) {
+        preSum[i][i] = nums[i];
+        if (preSum[i][i] === k) {
+            counter += 1;
+        }
+        for (let j = 0; j < i; j++) {
+            preSum[j][i] = preSum[j][i - 1] + nums[i];
+            if (preSum[j][i] === k) {
+                counter += 1;
+            }
+        }
+    }
+    return counter;
 };
 // @lc code=end
 
