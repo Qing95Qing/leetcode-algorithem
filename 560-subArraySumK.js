e409125e2458d7068c2ed779ed7cde5370227425
qing.qing <849832144@qq.com>
2024-05-17 15:07:01 +0800

    560-resolved

--- 560-subArraySumK.js
+++ 560-subArraySumK.js
@@ -8,6 +8,20 @@
  * [0, 2]
  */
 
+var subarraySum_mine_failed = function(nums, k) {
+    const preSum = new Array(nums.length).fill(0);
+    let counter = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j <= i; j++) {
+            preSum[j] = preSum[j] + nums[i];
+            if (preSum[j] === k) {
+                counter += 1;
+            }
+        }
+    }
+    return counter;
+};
 
 // @lc code=start
 /**
@@ -15,24 +29,26 @@
  * @param {number} k
  * @return {number}
  */
+// [1, 1, 1]
+// index: 0 => times: 0 map => { 1: 1 }
+// index: 1 => times: 0 map => { 1: 1 }
+
 var subarraySum = function(nums, k) {
-    const preSum = new Array(nums.length).fill(0).map(() => new Array(nums.length).fill(0));
-    preSum[0][0] = nums[0];
-    let counter = 0;
-    if (preSum[0][0] === k) {
-        counter += 1;
-    }
-    
-    for (let i = 1; i < nums.length; i++) {
-        preSum[i][i] = nums[i];
-        if (preSum[i][i] === k) {
-            counter += 1;
+    let sum = 0, counter = 0;
+    const map = new Map();
+    map.set(0, 1);
+    // preSum[i] - preSum[j - 1] = k => 有多少个 preSum[i] - k
+    for (let num of nums) {
+        sum += num;
+        // 判断当前i有多少个preSum[j - 1]
+        if (map.has(sum - k)) {
+            counter += map.get(sum - k);
         }
-        for (let j = 0; j < i; j++) {
-            preSum[j][i] = preSum[j][i - 1] + nums[i];
-            if (preSum[j][i] === k) {
-                counter += 1;
-            }
+        // 更新preSum[i]的记录
+        if (map.has(sum)) {
+            map.set(sum, map.get(sum) + 1);
+        } else {
+            map.set(sum, 1);
         }
     }
     return counter;
