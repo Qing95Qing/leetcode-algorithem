dc8b2c171863c3367b07fafa89411cf9b92c9134
qing.qing <849832144@qq.com>
2024-05-22 09:59:41 +0800

    64-solved

---
+++ 二维动态规划/10-regularMatch.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=10 lang=javascript
+ *
+ * [10] 正则表达式匹配
+ * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+
+ * '.' 匹配任意单个字符
+ * '*' 匹配零个或多个前面的那一个元素
+ * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+ * 字符串s为 "aa"
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s, p) {
+    let sIndex = 0, pIndex = 0;
+    while(sIndex < s.length && pIndex < p.length) {
+        if ()
+    }
+};
+// @lc code=end
+
