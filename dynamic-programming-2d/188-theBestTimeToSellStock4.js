b28ea3306a44f7b112f94d67800bb3b9b3d0efae
qing.qing <849832144@qq.com>
2024-06-05 16:40:27 +0800

    188-opt

--- dynamic-programming-2d/188-theBestTimeToSellStock4.js
+++ dynamic-programming-2d/188-theBestTimeToSellStock4.js
@@ -69,6 +69,26 @@ var maxProfit = function(k, prices) {
 };
 // @lc code=end
 
-// 15
-console.log(maxProfit(2, [1,7,4,2,11]))
+var maxProfit_opt = function(k, prices) {
+    if (!prices.length) {
+        return 0;
+    }
+
+    const n = prices.length;
+    k = Math.min(k, Math.floor(n / 2));
+    const buy = new Array(k + 1).fill(-Number.MAX_VALUE);
+    const sell = new Array(k + 1).fill(-Number.MAX_VALUE);
+
+    [buy[0], sell[0]] = [-prices[0], 0]
+
+    for (let i = 1; i < n; ++i) {
+        buy[0] = Math.max(buy[0], sell[0] - prices[i]);
+        for (let j = 1; j < k + 1; ++j) {
+            buy[j] = Math.max(buy[j], sell[j] - prices[i]);
+            sell[j] = Math.max(sell[j], buy[j - 1] + prices[i]); 
+        }
+    }
+    return Math.max(...sell)
+};
+console.log(maxProfit_opt(2, [1,7,4,2,11]))
 
