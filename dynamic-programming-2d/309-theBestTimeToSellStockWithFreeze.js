e40e2c4ea9179ba3a759a85e5104cb6176db299b
qing.qing <849832144@qq.com>
2024-06-12 14:11:53 +0800

    309-solved

--- dynamic-programming-2d/309-theBestTimeToSellStockWithFreeze.js
+++ dynamic-programming-2d/309-theBestTimeToSellStockWithFreeze.js
@@ -14,12 +14,16 @@
  * 解释: 对应的交易状态为: [买入, 卖出, 冷冻期, 买入, 卖出]
  * 
  * 相较于买卖股票2，多了冷冻期，增加了买入的限制条件，
- * 
- * 
- *          买1    买1卖1    买2卖1    买2卖2
- *   1day   -1
- *   2day   -2      1
- *   3day   -3     
+ * 第i天
+ * - 持有/买入：   [i-1](不持有/不操作) - prices[i]  
+ * - 持有/不操作： max([i-1](持有/买入), [i-1](持有/不操作))
+ * - 不持有/不操作： max([i-1](不持有/不操作), [i-1](不持有/卖出))
+ * - 不持有/卖出：  max([i-1](持有/买入), [i-1](持有/不操作)) + prices[i]
+ * [1, 2, 4]
+ *          持有/买入    持有/不操作    不持有/不操作     不持有/卖出    
+ *   1day     -1           -max          0           -max
+ *   2day     -2           -1            0            1
+ *   3day     -4           -1            1
  *   4day
  *   5day
  */
@@ -30,10 +34,29 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
+    let oneBuy = -prices[0], 
+        oneNo = -Number.MAX_VALUE,
+        zeroNo = 0,
+        zeroSell = -Number.MAX_VALUE;
 
+    for (let i = 1; i < prices.length; i++) {
+        let oneBuy1 = zeroNo -prices[i];
+        let oneNo1 = Math.max(oneBuy, oneNo);
+        let zeroNo1 = Math.max(zeroNo, zeroSell);
+        let zeroSell1 = Math.max(oneBuy, oneNo) + prices[i];
+
+        oneBuy = oneBuy1;
+        oneNo = oneNo1;
+        zeroNo = zeroNo1;
+        zeroSell = zeroSell1;
+    }
+
+    return Math.max(oneBuy, oneNo, zeroSell, zeroNo);
 };
 // @lc code=end
 
-console.log(maxProfit([1,2,3,0,2]))
+console.log(maxProfit([1,2,4]))
 // buy
+
+
 
